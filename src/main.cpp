a8b0ba90954fe6aee4c74674ccbfd824bb833f49
Connor McGhie <connor.mcghie@my.jcu.edu.au>
2026-06-03 09:41:48 +1000

    Move led driver files into dedicated folder

--- src/main.cpp
+++ src/main.cpp
@@ -19,55 +19,59 @@ int main()
     LEDDriver leds(pio0, 0);
     log(LogLevel::INFORMATION, "LED driver ready");
  
-    for (;;) {
-        // DEMO 1: Setting LEDs one at a time, then committing changes
-        // Stage multiple changes, then commit them all at once
-        leds.set(0, Colours::RED);
-        leds.set(1, Colours::GREEN);
-        leds.set(2, Colours::BLUE);
-        leds.show();
-        sleep_ms(1000);
+    // for (;;) {
+    //     // DEMO 1: Setting LEDs one at a time, then committing changes
+    //     // Stage multiple changes, then commit them all at once
+    //     leds.set(0, Colours::RED);
+    //     leds.set(1, Colours::GREEN);
+    //     leds.set(2, Colours::BLUE);
+    //     leds.show();
+    //     sleep_ms(1000);
  
-        // Stage a different pattern and commit
-        leds.set(0, Colours::BLUE);
-        leds.set(1, Colours::RED);
-        leds.set(2, Colours::GREEN);
-        leds.show();
-        sleep_ms(1000);
+    //     // Stage a different pattern and commit
+    //     leds.set(0, Colours::BLUE);
+    //     leds.set(1, Colours::RED);
+    //     leds.set(2, Colours::GREEN);
+    //     leds.show();
+    //     sleep_ms(1000);
  
-        // Clear everything and commit
-        leds.clear();
-        leds.show();
-        sleep_ms(500);
+    //     // Clear everything and commit
+    //     leds.clear();
+    //     leds.show();
+    //     sleep_ms(500);
 
-        // DEMO 2: Setting multiple LEDs at once, then committing changes
-        // Create array of changes to LEDs
-        LEDUpdate pattern[] = {
-            {0, Colours::RED},
-            {5, Colours::GREEN},
-            {11, Colours::BLUE},
-        };
+    //     // DEMO 2: Setting multiple LEDs at once, then committing changes
+    //     // Create array of changes to LEDs
+    //     LEDUpdate pattern[] = {
+    //         {0, Colours::RED},
+    //         {5, Colours::GREEN},
+    //         {11, Colours::BLUE},
+    //     };
         
-        // Parse changes into set_multiple method, then commit
-        leds.set_multiple(pattern, 3);
-        leds.show();
-        sleep_ms(1000);
+    //     // Parse changes into set_multiple method, then commit
+    //     leds.set_multiple(pattern, 3);
+    //     leds.show();
+    //     sleep_ms(1000);
  
-        // Clear and commit
-        leds.clear();
-        leds.show();
-        sleep_ms(500);
+    //     // Clear and commit
+    //     leds.clear();
+    //     leds.show();
+    //     sleep_ms(500);
 
-        // DEMO 3: Querying the colour of an LED (with visual feedback
-        // by setting to the same colour)
-        leds.set(0, Colours::RED);
-        Colour c = leds.get(0);
-        leds.set(1, c);
-        leds.show();
-        sleep_ms(1000);
+    //     // DEMO 3: Querying the colour of an LED (with visual feedback
+    //     // by setting to the same colour)
+    //     leds.set(0, Colours::RED);
+    //     Colour c = leds.get(0);
+    //     leds.set(1, c);
+    //     leds.show();
+    //     sleep_ms(1000);
 
-        leds.clear();
-        sleep_ms(1000);
+    //     leds.clear();
+    //     sleep_ms(1000);
+    // }
+
+    for (;;) {
+
     }
  
     return 0;
